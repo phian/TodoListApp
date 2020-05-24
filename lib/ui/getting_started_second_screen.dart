f0629035eddbd275c1eff526dcc48eab717a8528
angapkpro0123 <42203666+angapkpro0123@users.noreply.github.com>
2020-05-24 23:24:49 +0700

    Finished getting started screen UI and animation

--- lib/ui/getting_started_second_screen.dart
+++ lib/ui/getting_started_second_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:todoapp/presentation/forward_arrow_icon.dart';
 import 'package:todoapp/ui/getting_started_screen.dart';
